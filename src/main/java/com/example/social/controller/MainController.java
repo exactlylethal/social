ae286fcb012ed4a19e86ff2cf15b051c51ea3280
exactlylethal <p.kuriliv@yandex.kz>
2020-03-15 14:52:01 +0600

    Initial

---
+++ src/main/java/com/example/social/controller/MainController.java
@@ -0,0 +1,50 @@
+package com.example.social.controller;
+
+import com.example.social.domain.Message;
+import com.example.social.domain.User;
+import com.example.social.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+    @GetMapping("/main")
+    public String main(Map<String, Object> model){
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model){
+        Message message = new Message(text, tag, user);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        List<Message> messages = messageRepo.findByTag(filter);
+        model.put("messages", messages);
+        return "main";
+    }
+}
---
+++ src/main/java/com/example/social/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.social.repo;
+
+import com.example.social.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
